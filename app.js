5a7ec4209536ee55ee77c41fd6b4ad068adf8a14
shubham <hoodashubham92@gmail.com>
2023-08-10 23:53:37 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+// const prevBtn = document.getElementById("prev")
+// const nextBtn = document.getElementById("next")
+
+// prevBtn.addEventListener('click',()=>{
+//     let carousal = document.getElementsByClassName("carousal")
+//     // console.log(carousal[0].children[0].style);
+//     carousal[0].children[0].style.transform = "translate(-200px)"
+//     //  let images = document.getElementsByClassName("images")
+//     //  images.style.transform = "translateX(-200px)"
+// })
